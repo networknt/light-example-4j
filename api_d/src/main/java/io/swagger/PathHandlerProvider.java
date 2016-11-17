55c34e2bc7ada17fc88ea53090a559a69c1deed2
Steve Hu <stevehu@gmail.com>
2016-11-16 21:14:09 -0500

    checkin 4 apis

---
+++ api_d/src/main/java/io/swagger/PathHandlerProvider.java
@@ -0,0 +1,19 @@
+package io.swagger.handler;
+
+import com.networknt.config.Config;
+import com.networknt.server.HandlerProvider;
+import io.undertow.Handlers;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Methods;
+
+public class PathHandlerProvider implements HandlerProvider {
+
+    public HttpHandler getHandler() {
+        HttpHandler handler = Handlers.routing()
+            .add(Methods.GET, "/v1/data", new DataGetHandler())
+        ;
+        return handler;
+    }
+}
+
