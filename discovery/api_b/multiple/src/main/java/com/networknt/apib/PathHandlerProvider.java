a39af621d5daa32be0a3fb5ea289b3e4ad70fb1c
Steve Hu <stevehu@gmail.com>
2017-01-29 10:29:50 -0500

    add multiple

---
+++ discovery/api_b/multiple/src/main/java/com/networknt/apib/PathHandlerProvider.java
@@ -0,0 +1,19 @@
+package com.networknt.apib;
+
+import com.networknt.config.Config;
+import com.networknt.server.HandlerProvider;
+import io.undertow.Handlers;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Methods;
+import com.networknt.apib.handler.*;
+
+public class PathHandlerProvider implements HandlerProvider {
+    @Override
+    public HttpHandler getHandler() {
+        return Handlers.routing()
+            .add(Methods.GET, "/v1/data", new DataGetHandler())
+        ;
+    }
+}
+
