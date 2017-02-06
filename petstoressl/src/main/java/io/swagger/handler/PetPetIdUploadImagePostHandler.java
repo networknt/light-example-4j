4440d2d48967e476fb785e3000a0f4a28bebf005
Steve Hu <stevehu@gmail.com>
2017-02-06 11:19:27 -0500

    add petstoressl

---
+++ petstoressl/src/main/java/io/swagger/handler/PetPetIdUploadImagePostHandler.java
@@ -0,0 +1,22 @@
+package io.swagger.handler;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class PetPetIdUploadImagePostHandler implements HttpHandler {
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        Map<String, Object> examples = new HashMap<>();
+        examples.put("application/json", StringEscapeUtils.unescapeHtml4("{  &quot;code&quot; : 123,  &quot;type&quot; : &quot;aeiou&quot;,  &quot;message&quot; : &quot;aeiou&quot;}"));
+        if(examples.size() > 0) {
+            exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
+            exchange.getResponseSender().send((String)examples.get("application/json"));
+        } else {
+            exchange.endExchange();
+        }
+    }
+}
