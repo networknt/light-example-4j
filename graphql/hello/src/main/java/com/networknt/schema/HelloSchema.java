723daf3ee1f7e3a43caa025e85c6b02182e97ee6
Steve Hu <stevehu@gmail.com>
2017-03-25 22:39:19 -0400

    add one graphql hello example

---
+++ graphql/hello/src/main/java/com/networknt/schema/HelloSchema.java
@@ -0,0 +1,29 @@
+package com.networknt.schema;
+
+import com.networknt.graphql.router.SchemaProvider;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
+
+import static graphql.Scalars.GraphQLString;
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+import static graphql.schema.GraphQLObjectType.newObject;
+
+/**
+ * Created by steve on 25/03/17.
+ */
+public class HelloSchema implements SchemaProvider {
+    @Override
+    public GraphQLSchema getSchema() {
+        GraphQLObjectType queryType = newObject()
+                .name("helloWorldQuery")
+                .field(newFieldDefinition()
+                        .type(GraphQLString)
+                        .name("hello")
+                        .staticValue("world"))
+                .build();
+
+        return GraphQLSchema.newSchema()
+                .query(queryType)
+                .build();
+    }
+}
