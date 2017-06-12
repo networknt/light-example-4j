d548e54dd9f0acc90012ba607d363a4f51e1991a
Steve Hu <stevehu@gmail.com>
2017-06-11 20:01:02 -0400

    add httpchain folders

---
+++ rest/ms_chain/api_b/httpchain/src/main/java/com/networknt/apib/handler/DataGetHandler.java
@@ -0,0 +1,49 @@
+
+package com.networknt.apib.handler;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.networknt.client.Client;
+import com.networknt.config.Config;
+import com.networknt.exception.ClientException;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataGetHandler implements HttpHandler {
+    static String CONFIG_NAME = "api_b";
+    static String apidUrl = (String) Config.getInstance().getJsonMapConfig(CONFIG_NAME).get("api_c_endpoint");
+
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        List<String> list = new ArrayList<String>();
+        try {
+            CloseableHttpClient client = Client.getInstance().getSyncClient();
+            HttpGet httpGet = new HttpGet(apidUrl);
+            CloseableHttpResponse response = client.execute(httpGet);
+            int responseCode = response.getStatusLine().getStatusCode();
+            if(responseCode != 200){
+                throw new Exception("Failed to call API C: " + responseCode);
+            }
+            List<String> apicList = (List<String>) Config.getInstance().getMapper().readValue(response.getEntity().getContent(),
+                    new TypeReference<List<String>>(){});
+            list.addAll(apicList);
+        } catch (ClientException e) {
+            throw new Exception("Client Exception: ", e);
+        } catch (IOException e) {
+            throw new Exception("IOException:", e);
+        }
+        // now add API B specific messages
+        list.add("API B: Message 1");
+        list.add("API B: Message 2");
+        exchange.getResponseSender().send(Config.getInstance().getMapper().writeValueAsString(list));
+    }
+}
