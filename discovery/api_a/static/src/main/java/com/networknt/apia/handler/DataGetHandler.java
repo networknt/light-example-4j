b725d293706b639ad30b6009ed19c3f0208ee526
Steve Hu <stevehu@gmail.com>
2017-06-24 10:23:22 -0400

    complete static

--- discovery/api_a/static/src/main/java/com/networknt/apia/handler/DataGetHandler.java
+++ discovery/api_a/static/src/main/java/com/networknt/apia/handler/DataGetHandler.java
@@ -1,18 +1,76 @@
 
 package com.networknt.apia.handler;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.networknt.client.Client;
+import com.networknt.config.Config;
+import com.networknt.exception.ClientException;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HttpString;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
 
 public class DataGetHandler implements HttpHandler {
+    static String CONFIG_NAME = "api_a";
+    static String apibUrl = (String)Config.getInstance().getJsonMapConfig(CONFIG_NAME).get("api_b_endpoint");
+    static String apicUrl = (String) Config.getInstance().getJsonMapConfig(CONFIG_NAME).get("api_c_endpoint");
+
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        
-            exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
-             exchange.getResponseSender().send(" [\"Message 1\",\"Message 2\"]");
-        
+        List<String> list = new Vector<String>();
+        final HttpGet[] requests = new HttpGet[] {
+                new HttpGet(apibUrl),
+                new HttpGet(apicUrl),
+        };
+        try {
+            CloseableHttpAsyncClient client = Client.getInstance().getAsyncClient();
+            final CountDownLatch latch = new CountDownLatch(requests.length);
+            for (final HttpGet request: requests) {
+                //Client.getInstance().propagateHeaders(request, exchange);
+                client.execute(request, new FutureCallback<HttpResponse>() {
+                    @Override
+                    public void completed(final HttpResponse response) {
+                        try {
+                            List<String> apiList = Config.getInstance().getMapper().readValue(response.getEntity().getContent(),
+                                    new TypeReference<List<String>>(){});
+                            list.addAll(apiList);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        latch.countDown();
+                    }
+
+                    @Override
+                    public void failed(final Exception ex) {
+                        ex.printStackTrace();
+                        latch.countDown();
+                    }
+
+                    @Override
+                    public void cancelled() {
+                        System.out.println("cancelled");
+                        latch.countDown();
+                    }
+                });
+            }
+            latch.await();
+        } catch (ClientException e) {
+            e.printStackTrace();
+            throw new Exception("ClientException:", e);
+        }
+        // now add API A specific messages
+        list.add("API A: Message 1");
+        list.add("API A: Message 2");
+        exchange.getResponseSender().send(Config.getInstance().getMapper().writeValueAsString(list));
     }
 }
