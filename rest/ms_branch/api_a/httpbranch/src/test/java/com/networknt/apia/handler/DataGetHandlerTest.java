b114a93eb680c02f581b53ef261d8f947bdfb687
Steve Hu <stevehu@gmail.com>
2017-07-12 21:42:43 -0400

    redo the ms_branch tutorial

---
+++ rest/ms_branch/api_a/httpbranch/src/test/java/com/networknt/apia/handler/DataGetHandlerTest.java
@@ -0,0 +1,44 @@
+
+package com.networknt.apia.handler;
+
+import com.networknt.client.Client;
+import com.networknt.server.Server;
+import com.networknt.exception.ClientException;
+import com.networknt.exception.ApiException;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+
+public class DataGetHandlerTest {
+    @ClassRule
+    public static TestServer server = TestServer.getInstance();
+
+    static final Logger logger = LoggerFactory.getLogger(DataGetHandlerTest.class);
+
+    @Test
+    public void testDataGetHandlerTest() throws ClientException, ApiException {
+        CloseableHttpClient client = Client.getInstance().getSyncClient();
+        HttpGet httpGet = new HttpGet ("http://localhost:8080/v1/data");
+        /*
+        Client.getInstance().addAuthorization(httpGet);
+        try {
+            CloseableHttpResponse response = client.execute(httpGet);
+            int statusCode = response.getStatusLine().getStatusCode();
+            String body = IOUtils.toString(response.getEntity().getContent(), "utf8");
+            Assert.assertEquals(200, statusCode);
+            Assert.assertEquals("", body);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+    }
+}
