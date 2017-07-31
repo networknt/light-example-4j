a7afa3dd758118cf10d069b0a4133e63e591259a
Steve Hu <stevehu@gmail.com>
2017-07-31 18:56:45 -0400

    add hybrid-command and hybrid-query services

---
+++ eventuate/todo-list/hybrid-command/src/main/java/com/networknt/todolist/command/handler/DeleteTodo.java
@@ -0,0 +1,46 @@
+
+package com.networknt.todolist.command.handler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.networknt.config.Config;
+import com.networknt.eventuate.common.AggregateRepository;
+import com.networknt.eventuate.common.EventuateAggregateStore;
+import com.networknt.eventuate.todolist.TodoCommandService;
+import com.networknt.eventuate.todolist.TodoCommandServiceImpl;
+import com.networknt.eventuate.todolist.common.model.TodoInfo;
+import com.networknt.eventuate.todolist.domain.TodoAggregate;
+import com.networknt.eventuate.todolist.domain.TodoBulkDeleteAggregate;
+import com.networknt.service.SingletonServiceFactory;
+import com.networknt.utility.NioUtils;
+import com.networknt.rpc.Handler;
+import com.networknt.rpc.router.ServiceHandler;
+import java.nio.ByteBuffer;
+import java.util.concurrent.CompletableFuture;
+
+@ServiceHandler(id="lightapi.net/todo/delete/0.1.0")
+public class DeleteTodo implements Handler {
+
+    private EventuateAggregateStore eventStore  = (EventuateAggregateStore) SingletonServiceFactory.getBean(EventuateAggregateStore.class);
+
+    private AggregateRepository todoRepository = new AggregateRepository(TodoAggregate.class, eventStore);
+    private AggregateRepository bulkDeleteAggregateRepository  = new AggregateRepository(TodoBulkDeleteAggregate.class, eventStore);
+
+    private TodoCommandService service = new TodoCommandServiceImpl(todoRepository, bulkDeleteAggregateRepository);
+
+
+    @Override
+    public ByteBuffer handle(Object input)  {
+
+        JsonNode inputPara = Config.getInstance().getMapper().valueToTree(input);
+        // delete a todo-event
+        String id = inputPara.findPath("id").asText();
+
+        CompletableFuture<TodoInfo> result = service.remove(id).thenApply((e) -> {
+            TodoInfo m = e.getAggregate().getTodo();
+            return m;
+        });
+
+
+        return NioUtils.toByteBuffer("{\"message\":" + result + "}");
+    }
+}
