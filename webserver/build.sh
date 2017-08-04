993fe063f3684e20c878eb1a7abe06edb7a35e90
Steve Hu <stevehu@gmail.com>
2017-08-03 22:10:08 -0400

    add build.sh for docker build and push

---
+++ webserver/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+CURRENT_DIR="${PWD##*/}"
+IMAGE_NAME="networknt/$CURRENT_DIR"
+TAG="${1}"
+
+if docker inspect ${IMAGE_NAME} &> /dev/null; then
+	docker rmi -f ${IMAGE_NAME}:latest
+	docker rmi -f ${IMAGE_NAME}:${TAG}
+fi
+
+docker build -t ${IMAGE_NAME} .
+docker tag ${IMAGE_NAME} ${IMAGE_NAME}:latest
+docker tag ${IMAGE_NAME} ${IMAGE_NAME}:${TAG}
+docker push ${IMAGE_NAME}
