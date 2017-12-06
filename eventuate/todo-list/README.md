685faf17074071d9818a8399ce0f54ea4663c3d5
Steve Hu <stevehu@gmail.com>
2017-12-06 12:08:34 -0500

    update logging

--- eventuate/todo-list/README.md
+++ eventuate/todo-list/README.md
@@ -21,3 +21,29 @@ curl -k -H "Content-Type: application/json" -X POST   https://localhost:8081/v1/
 
 ### Hybrid services
 
+
+
+### Check message on Kafka
+
+The first step is to get into the container of kafka. First use docker ps to find
+the container id for Kafka. And then use that container id to get into the container.
+
+```
+docker ps
+docker exec -it 90472bd69381 bash
+```
+
+
+check how many topics in kafka
+
+
+```
+./bin/kafka-topics.sh --zookeeper zookeeper:2181 --list
+```
+
+check kafka topic for todo-list
+
+```
+./bin/kafka-console-consumer.sh --zookeeper zookeeper:2181 --topic com.networknt.eventuate.todolist.domain.TodoAggregate --from-beginning
+```
+
--- eventuate/todo-list/query/src/main/java/com/networknt/eventuate/todolist/TodoQueryWorkflow.java
+++ eventuate/todo-list/query/src/main/java/com/networknt/eventuate/todolist/TodoQueryWorkflow.java
@@ -9,11 +9,13 @@ import com.networknt.eventuate.todolist.common.event.TodoDeletedEvent;
 import com.networknt.eventuate.todolist.common.event.TodoUpdatedEvent;
 import com.networknt.eventuate.todolist.common.model.TodoInfo;
 import com.networknt.service.SingletonServiceFactory;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @EventSubscriber(id = "todoQuerySideEventHandlers")
 public class TodoQueryWorkflow {
+    static final private Logger logger = LoggerFactory.getLogger(TodoQueryWorkflow.class);
 
     private TodoQueryService service =
             (TodoQueryService)SingletonServiceFactory.getBean(TodoQueryService.class);
@@ -23,17 +25,20 @@ public class TodoQueryWorkflow {
 
     @EventHandlerMethod
     public void create(DispatchedEvent<TodoCreatedEvent> de) {
+        logger.debug("TodoCreateEvent received " + de);
         TodoInfo todo = de.getEvent().getTodo();
         service.save(de.getEntityId(), todo);
     }
 
     @EventHandlerMethod
     public void delete(DispatchedEvent<TodoDeletedEvent> de) {
+        logger.debug("TodoDeletedEvent received " + de);
         service.remove(de.getEntityId());
     }
 
     @EventHandlerMethod
     public void update(DispatchedEvent<TodoUpdatedEvent> de) {
+        logger.debug("TodoUpdatedEvent received " + de);
         TodoInfo todo = de.getEvent().getTodo();
         service.save(de.getEntityId(), todo);
     }
