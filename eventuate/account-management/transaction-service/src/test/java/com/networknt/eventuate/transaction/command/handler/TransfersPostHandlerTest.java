afce1c947351fb1e171af33061a7943fa0b5ac3e
chenyan71 <passWord11!!>
2017-12-07 13:49:50 -0500

    new verison for the example-1.5.4

--- eventuate/account-management/transaction-service/src/test/java/com/networknt/eventuate/transaction/command/handler/TransfersPostHandlerTest.java
+++ eventuate/account-management/transaction-service/src/test/java/com/networknt/eventuate/transaction/command/handler/TransfersPostHandlerTest.java
@@ -1,13 +1,24 @@
 
 package com.networknt.eventuate.transaction.command.handler;
 
-import com.networknt.client.Client;
+
+import com.networknt.client.Http2Client;
 import com.networknt.eventuate.account.common.model.account.CreateAccountRequest;
+import com.networknt.eventuate.account.common.model.customer.Address;
+import com.networknt.eventuate.account.common.model.customer.CustomerInfo;
+import com.networknt.eventuate.account.common.model.customer.Name;
+import com.networknt.eventuate.account.common.model.customer.UserCredentials;
 import com.networknt.eventuate.account.common.model.transaction.CreateMoneyTransferRequest;
 import com.networknt.eventuate.common.impl.JSonMapper;
 import com.networknt.server.Server;
 import com.networknt.exception.ClientException;
 import com.networknt.exception.ApiException;
+import io.undertow.UndertowOptions;
+import io.undertow.client.ClientConnection;
+import io.undertow.client.ClientRequest;
+import io.undertow.client.ClientResponse;
+import io.undertow.util.Headers;
+import io.undertow.util.Methods;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -18,9 +29,14 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xnio.IoUtils;
+import org.xnio.OptionMap;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class TransfersPostHandlerTest {
@@ -28,28 +44,49 @@ public class TransfersPostHandlerTest {
     public static TestServer server = TestServer.getInstance();
 
     static final Logger logger = LoggerFactory.getLogger(TransfersPostHandlerTest.class);
-
+    static final boolean enableHttp2 = server.getServerConfig().isEnableHttp2();
+    static final boolean enableHttps = server.getServerConfig().isEnableHttps();
+    static final int httpPort = server.getServerConfig().getHttpPort();
+    static final int httpsPort = server.getServerConfig().getHttpsPort();
+    static final String url = enableHttp2 || enableHttps ? "https://localhost:" + httpsPort : "http://localhost:" + httpPort;
     @Test
     public void testTransfersPostHandlerTest() throws ClientException, ApiException {
-        CloseableHttpClient client = Client.getInstance().getSyncClient();
-        HttpPost httpPost = new HttpPost ("http://localhost:8085/v1/transfers");
-        CreateMoneyTransferRequest request = new CreateMoneyTransferRequest();
-        request.setFromAccountId("2222-22222");
-        request.setToAccountId("2222-22223");
-        request.setAmount(new BigDecimal(23555L));
-        request.setDescription("test");
-        String json = JSonMapper.toJson(request);
-        System.out.println(json);
-      //  Client.getInstance().addAuthorization(httpPost);
+
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
         try {
-            httpPost.setEntity(new StringEntity(json));
-            httpPost.setHeader("Content-type", "application/json");
-            CloseableHttpResponse response = client.execute(httpPost);
-            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
-            //Assert.assertEquals("", IOUtils.toString(response.getEntity().getContent(), "utf8"));
+            connection = client.connect(new URI(url), Http2Client.WORKER, Http2Client.SSL, Http2Client.POOL, enableHttp2 ? OptionMap.create(UndertowOptions.ENABLE_HTTP2, true): OptionMap.EMPTY).get();
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new ClientException(e);
         }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        CreateMoneyTransferRequest transferRequest = new CreateMoneyTransferRequest();
+        transferRequest.setFromAccountId("2222-22222");
+        transferRequest.setToAccountId("2222-22223");
+        transferRequest.setAmount(new BigDecimal(23555L));
+        transferRequest.setDescription("test");
+        String json = JSonMapper.toJson(transferRequest);
+        try {
+            ClientRequest request = new ClientRequest().setPath("/v1/transfers").setMethod(Methods.POST);
+
+            request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/json");
+            request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+            connection.sendRequest(request, client.createClientCallback(reference, latch, json));
+
+            latch.await();
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        System.out.println("response:" + body);
+        Assert.assertEquals(200, statusCode);
+        Assert.assertNotNull(body);
+
 
     }
 }
