d2e96fbe4451269208dd54bce17e6ce62bb2dda7
Gavin Chen <gavinyan.han@gmail.com>
2017-12-20 14:22:58 -0500

    fix the pom dependency issues

--- tram/light-tram-todolist/src/main/java/com/networknt/tram/todolist/command/TodoNotFoundException.java
+++ tram/light-tram-todolist/src/main/java/com/networknt/tram/todolist/command/TodoNotFoundException.java
@@ -2,6 +2,6 @@ package com.networknt.tram.todolist.command;
 
 public class TodoNotFoundException extends RuntimeException {
 	public TodoNotFoundException(String id) {
-		super(String.format("Todo with id=%s not found"));
+		super(String.format("Todo with id=%s not found", id));
 	}
 }
