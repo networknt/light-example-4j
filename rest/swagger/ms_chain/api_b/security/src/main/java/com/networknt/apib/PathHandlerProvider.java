0c8e54a461e0ea4a7befa1792ea3f257ef669538
Steve Hu <stevehu@gmail.com>
2017-12-20 15:52:37 -0500

    add ms_chain security

---
+++ rest/swagger/ms_chain/api_b/security/src/main/java/com/networknt/apib/PathHandlerProvider.java
@@ -0,0 +1,27 @@
+
+package com.networknt.apib;
+
+import com.networknt.config.Config;
+import com.networknt.server.HandlerProvider;
+import io.undertow.Handlers;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Methods;
+import com.networknt.info.ServerInfoGetHandler;
+import com.networknt.health.HealthGetHandler;
+import com.networknt.apib.handler.*;
+
+public class PathHandlerProvider implements HandlerProvider {
+    @Override
+    public HttpHandler getHandler() {
+        return Handlers.routing()
+        
+            .add(Methods.GET, "/v1/data", new DataGetHandler())
+        
+            .add(Methods.GET, "/v1/health", new HealthGetHandler())
+        
+            .add(Methods.GET, "/v1/server/info", new ServerInfoGetHandler())
+        
+        ;
+    }
+}
