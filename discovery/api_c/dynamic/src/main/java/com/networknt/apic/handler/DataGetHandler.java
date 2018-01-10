f50a5bebab9a62e18d3c877647f70f4beadbbcd9
Steve Hu <stevehu@gmail.com>
2018-01-10 17:55:25 -0500

    complate dynamic for discovery

---
+++ discovery/api_c/dynamic/src/main/java/com/networknt/apic/handler/DataGetHandler.java
@@ -0,0 +1,18 @@
+package com.networknt.apic.handler;
+
+import com.networknt.config.Config;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataGetHandler implements HttpHandler {
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        List<String> messages = new ArrayList<>();
+        messages.add("API C: Message 1");
+        messages.add("API C: Message 2");
+        exchange.getResponseSender().send(Config.getInstance().getMapper().writeValueAsString(messages));
+    }
+}
