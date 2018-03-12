65ea2f0c3ed39c447370661a5de27ee6339f7ee9
Steve Hu <stevehu@gmail.com>
2018-03-11 20:33:33 -0400

    checkin 4 apis for OpenAPI ms-aggregate

---
+++ rest/openapi/ms-aggregate/ab/generated/src/main/java/com/networknt/ab/PathHandlerProvider.java
@@ -0,0 +1,28 @@
+
+package com.networknt.ab;
+
+import com.networknt.config.Config;
+import com.networknt.server.HandlerProvider;
+import io.undertow.Handlers;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Methods;
+import com.networknt.info.ServerInfoGetHandler;
+import com.networknt.health.HealthGetHandler;
+
+import com.networknt.ab.handler.*;
+
+public class PathHandlerProvider implements HandlerProvider {
+    @Override
+    public HttpHandler getHandler() {
+        return Handlers.routing()
+        
+            .add(Methods.GET, "/v1/health", new HealthGetHandler())
+        
+            .add(Methods.GET, "/v1/server/info", new ServerInfoGetHandler())
+        
+            .add(Methods.GET, "/v1/data", new DataGetHandler())
+        
+        ;
+    }
+}
---
+++ rest/openapi/ms-aggregate/ad/generated/src/main/java/com/networknt/ad/handler/DataGetHandler.java
@@ -0,0 +1,18 @@
+
+package com.networknt.ad.handler;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataGetHandler implements HttpHandler {
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        
+            exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
+             exchange.getResponseSender().send("[\"Message 1\",\"Message 2\"]");
+        
+    }
+}
