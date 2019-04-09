0ffc23f4270bad3fba8be8874425c11951c50325
Steve Hu <stevehu@gmail.com>
2019-04-08 22:09:07 -0400

    fixes #97 regenerate light-hybrid-4j generic-server and generic service

---
+++ hybrid/generic-server/docker/Dockerfile-Redhat
@@ -0,0 +1,5 @@
+
+FROM registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift
+#EXPOSE  8443
+ADD /target/gserver-1.0.0.jar server.jar
+CMD ["/bin/sh","-c","java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Dlight-4j-config-dir=/config -Dlogback.configurationFile=/config/logback.xml -jar server.jar"]
--- hybrid/generic-service/src/main/java/com/networknt/gservice/handler/HelloWorld1.java
+++ hybrid/generic-service/src/main/java/com/networknt/gservice/handler/HelloWorld1.java
@@ -5,11 +5,12 @@ import com.networknt.utility.NioUtils;
 import com.networknt.rpc.Handler;
 import com.networknt.rpc.router.ServiceHandler;
 import java.nio.ByteBuffer;
+import io.undertow.server.HttpServerExchange;
 
 @ServiceHandler(id="lightapi.net/world/hello/0.1.0")
 public class HelloWorld1 implements Handler {
     @Override
-    public ByteBuffer handle(Object input)  {
+    public ByteBuffer handle(HttpServerExchange exchange, Object input)  {
         return NioUtils.toByteBuffer("{\"message\":\"Hello World!\"}");
     }
 }
