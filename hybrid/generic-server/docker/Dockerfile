3ab735b99826c7eac52564b76f7a40ef01aad079
Steve Hu <stevehu@gmail.com>
2019-04-08 22:09:07 -0400

    update generic server pom.xml

---
+++ hybrid/generic-server/docker/Dockerfile
@@ -0,0 +1,5 @@
+
+FROM openjdk:8-jre-alpine
+#EXPOSE  8443
+ADD /target/gserver-1.0.0.jar server.jar
+CMD ["/bin/sh","-c","java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Dlight-4j-config-dir=/config -Dlogback.configurationFile=/config/logback.xml -cp /server.jar:/service/* com.networknt.server.Server"]
--- hybrid/generic-service/src/main/java/com/networknt/gservice/handler/WelcomeWorld.java
+++ hybrid/generic-service/src/main/java/com/networknt/gservice/handler/WelcomeWorld.java
@@ -5,11 +5,12 @@ import com.networknt.utility.NioUtils;
 import com.networknt.rpc.Handler;
 import com.networknt.rpc.router.ServiceHandler;
 import java.nio.ByteBuffer;
+import io.undertow.server.HttpServerExchange;
 
 @ServiceHandler(id="lightapi.net/world/welcome/0.0.1")
 public class WelcomeWorld implements Handler {
     @Override
-    public ByteBuffer handle(Object input)  {
+    public ByteBuffer handle(HttpServerExchange exchange, Object input)  {
         return NioUtils.toByteBuffer("{\"message\":\"Hello World!\"}");
     }
 }
