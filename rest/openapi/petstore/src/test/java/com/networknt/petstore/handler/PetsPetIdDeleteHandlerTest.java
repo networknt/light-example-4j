65514a862c6e9c85d4b7453dabdabb5644ec1a31
Steve Hu <stevehu@gmail.com>
2019-05-20 15:29:52 -0400

    upgrade petstore with the 1.6.2 light-codegen

--- rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsPetIdDeleteHandlerTest.java
+++ rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsPetIdDeleteHandlerTest.java
@@ -54,13 +54,13 @@ public class PetsPetIdDeleteHandlerTest {
             throw new ClientException(e);
         }
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
-        String requestUri = "/v1/pets/uclOLsKHBYQkSzRNYOKjrnR";
+        String requestUri = "/v1/pets/fNFfbawSlLvPHMlzribwF";
         String httpMethod = "delete";
         try {
             ClientRequest request = new ClientRequest().setPath(requestUri).setMethod(Methods.DELETE);
             
             //customized header parameters 
-            request.getRequestHeaders().put(new HttpString("key"), "kwGolTRXtrNLvqJTDTMoNVFedHGo");
+            request.getRequestHeaders().put(new HttpString("key"), "kDjUm");
             connection.sendRequest(request, client.createClientCallback(reference, latch));
             
             latch.await();
