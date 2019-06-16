bedf9fc6e0c7d52acb91b2f2fbf120f5241360ec
Steve Hu <stevehu@gmail.com>
2019-06-15 22:46:29 -0400

    update petstore to 2.0.2-SNAPSHOT

--- rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsPetIdGetHandlerTest.java
+++ rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsPetIdGetHandlerTest.java
@@ -54,7 +54,7 @@ public class PetsPetIdGetHandlerTest {
             throw new ClientException(e);
         }
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
-        String requestUri = "/v1/pets/bqqzJCIDOVXRjSCeDyjtD";
+        String requestUri = "/v1/pets/HWtbGdRHxhjfhmwYphgPIKpVL";
         String httpMethod = "get";
         try {
             ClientRequest request = new ClientRequest().setPath(requestUri).setMethod(Methods.GET);
