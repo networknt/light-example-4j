57b4f793e0e3ab9a352209cec6c4b23bd15564a5
Steve Hu <stevehu@gmail.com>
2019-06-20 15:52:40 -0400

    fixes #115 redo service discovery tutorial based on the 2.0.x release

---
+++ discovery/api_b/generated/src/main/java/com/networknt/ab/handler/DataGetHandler.java
@@ -0,0 +1,17 @@
+package com.networknt.ab.handler;
+
+import com.networknt.handler.LightHttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataGetHandler implements LightHttpHandler {
+    
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
+        exchange.setStatusCode(200);
+        exchange.getResponseSender().send("[\"Message 1\",\"Message 2\"]");
+    }
+}
