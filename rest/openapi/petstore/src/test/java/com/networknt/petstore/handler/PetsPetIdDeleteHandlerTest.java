9a01fc91d3d8f51cf58b5bfd73fb1568b3c997b6
Steve Hu <stevehu@gmail.com>
2020-02-05 09:54:58 -0500

    regnerate the openapi petstore

--- rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsPetIdDeleteHandlerTest.java
+++ rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsPetIdDeleteHandlerTest.java
@@ -54,13 +54,13 @@ public class PetsPetIdDeleteHandlerTest {
             throw new ClientException(e);
         }
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
-        String requestUri = "/v1/pets/RzXtXWIqNRUwljaszJOlZnqgSs";
+        String requestUri = "/v1/pets/aWTyCDjyeauBGxyNHkv";
         String httpMethod = "delete";
         try {
             ClientRequest request = new ClientRequest().setPath(requestUri).setMethod(Methods.DELETE);
             
             //customized header parameters 
-            request.getRequestHeaders().put(new HttpString("key"), "dLIkzr");
+            request.getRequestHeaders().put(new HttpString("key"), "OBDFXqvzA");
             connection.sendRequest(request, client.createClientCallback(reference, latch));
             
             latch.await();
