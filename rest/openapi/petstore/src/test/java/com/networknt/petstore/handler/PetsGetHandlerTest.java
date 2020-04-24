74603e3234539d8ea0c5f2d99dbb6e201d64e360
Steve Hu <stevehu@gmail.com>
2020-04-23 21:13:44 -0400

    fixes #612 regenerate OpenAPI petstore with the latest generator

--- rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsGetHandlerTest.java
+++ rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsGetHandlerTest.java
@@ -54,7 +54,7 @@ public class PetsGetHandlerTest {
             throw new ClientException(e);
         }
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
-        String requestUri = "/v1/pets?limit=5";
+        String requestUri = "/v1/pets?limit=10";
         String httpMethod = "get";
         try {
             ClientRequest request = new ClientRequest().setPath(requestUri).setMethod(Methods.GET);
