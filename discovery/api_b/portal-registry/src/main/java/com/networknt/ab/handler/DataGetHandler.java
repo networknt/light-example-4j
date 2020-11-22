e03fd96a86c5001f764422a1b0f4e34b252bcf3b
Steve Hu <stevehu@gmail.com>
2020-11-22 13:05:58 -0500

    fixes #635 add portal-registry for the discovery APIs

---
+++ discovery/api_b/portal-registry/src/main/java/com/networknt/ab/handler/DataGetHandler.java
@@ -0,0 +1,80 @@
+package com.networknt.ab.handler;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.networknt.client.Http2Client;
+import com.networknt.cluster.Cluster;
+import com.networknt.config.Config;
+import com.networknt.exception.ClientException;
+import com.networknt.handler.LightHttpHandler;
+import com.networknt.security.JwtVerifier;
+import com.networknt.server.Server;
+import com.networknt.service.SingletonServiceFactory;
+import io.undertow.UndertowOptions;
+import io.undertow.client.ClientConnection;
+import io.undertow.client.ClientRequest;
+import io.undertow.client.ClientResponse;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import io.undertow.util.Methods;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xnio.OptionMap;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class DataGetHandler implements LightHttpHandler {
+    static Logger logger = LoggerFactory.getLogger(DataGetHandler.class);
+    static Cluster cluster = SingletonServiceFactory.getBean(Cluster.class);
+    static String apidHost;
+    static String path = "/v1/data";
+    static Map<String, Object> securityConfig = (Map)Config.getInstance().getJsonMapConfig(JwtVerifier.SECURITY_CONFIG);
+    static boolean securityEnabled = (Boolean)securityConfig.get(JwtVerifier.ENABLE_VERIFY_JWT);
+    static String tag = Server.getServerConfig().getEnvironment();
+
+    static Http2Client client = Http2Client.getInstance();
+
+    public DataGetHandler() {
+    }
+
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        List<String> list = new ArrayList<>();
+        final CountDownLatch latch = new CountDownLatch(1);
+        ClientConnection connection = null;
+        try {
+            apidHost = cluster.serviceToUrl("https", "com.networknt.ad-1.0.0", tag, null);
+            connection = client.connect(new URI(apidHost), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.create(UndertowOptions.ENABLE_HTTP2, true)).get();
+        } catch (Exception e) {
+            logger.error("Exeption:", e);
+            throw new ClientException(e);
+        }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        try {
+            ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath(path);
+            // this is to ask client module to pass through correlationId and traceabilityId as well as
+            // getting access token from oauth2 server automatically and attatch authorization headers.
+            if(securityEnabled) client.propagateHeaders(request, exchange);
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+            int statusCode = reference.get().getResponseCode();
+            if(statusCode >= 300){
+                throw new Exception("Failed to call API D: " + statusCode);
+            }
+            List<String> apidList = Config.getInstance().getMapper().readValue(reference.get().getAttachment(Http2Client.RESPONSE_BODY),
+                    new TypeReference<List<String>>(){});
+            list.addAll(apidList);
+        } catch (Exception e) {
+            logger.error("Exception:", e);
+            throw new ClientException(e);
+        }
+        list.add("API B: Message 1");
+        list.add("API B: Message 2");
+        exchange.getResponseSender().send(Config.getInstance().getMapper().writeValueAsString(list));
+    }
+}
