87616ffc015a3bc90b11ecf4a9b2a176f1ee7485
Steve Hu <stevehu@gmail.com>
2020-12-18 12:42:07 -0500

    fixes #645 add aws lambda examples with both zip and image package types

---
+++ lambda/petstore-zip/PetsGetFunction/src/main/java/com/networknt/petstore/handler/App.java
@@ -0,0 +1,45 @@
+
+package com.networknt.petstore.handler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.networknt.aws.lambda.LightRequestHandler;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App extends LightRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        /* Generated Begin */
+        APIGatewayProxyResponseEvent response = interceptRequest(input, context);
+        if(response != null) {
+            return interceptResponse(response);
+        }
+        /* Generated End */
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        response = new APIGatewayProxyResponseEvent().withHeaders(headers);
+        String output = "";
+        output = "[{\"id\":1,\"name\":\"catten\",\"tag\":\"cat\"},{\"id\":2,\"name\":\"doggy\",\"tag\":\"dog\"}]";
+        response.withStatusCode(200)
+                .withBody(output);
+
+        /* Generated Begin */
+        return interceptResponse(response);
+        /* Generated End */
+    }
+}
