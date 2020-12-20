a8fd0c23e1ccae625576646933e26bcc9691f536
Steve Hu <stevehu@gmail.com>
2020-12-20 15:40:37 -0500

    fixes #646 update lambda petstore-zip with new generator for OpenAPI spec

--- lambda/petstore-zip/PetsPostFunction/src/main/java/com/networknt/petstore/handler/App.java
+++ lambda/petstore-zip/PetsPostFunction/src/main/java/com/networknt/petstore/handler/App.java
@@ -1,29 +1,32 @@
 
 package com.networknt.petstore.handler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.networknt.aws.lambda.LightRequestHandler;
-import java.util.HashMap;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class App extends LightRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         /* Generated Begin */
+        try {
+            if(logger.isDebugEnabled()) logger.debug(objectMapper.writeValueAsString(input));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         APIGatewayProxyResponseEvent response = interceptRequest(input, context);
         if(response != null) {
             return interceptResponse(response);
