21e65ef11537a47fcf1c0970ce908424337e5f21
Steve Hu <stevehu@gmail.com>
2021-01-02 21:44:50 -0500

    fixes #647 add kafka example folder and producer-consumer example

---
+++ kafka/producer-consumer/src/main/java/com/networknt/kafka/handler/ConsumerTopicGetHandler.java
@@ -0,0 +1,19 @@
+package com.networknt.kafka.handler;
+
+import com.networknt.handler.LightHttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+For more information on how to write business handlers, please check the link below.
+https://doc.networknt.com/development/business-handler/rest/
+*/
+public class ConsumerTopicGetHandler implements LightHttpHandler {
+    
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        exchange.endExchange();
+    }
+}
