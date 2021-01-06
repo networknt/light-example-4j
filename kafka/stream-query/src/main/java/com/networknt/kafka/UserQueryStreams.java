b86f366d0f1fb27c39271e676a333ae7ba57000b
Steve Hu <stevehu@gmail.com>
2021-01-06 17:07:22 -0500

    fixes #651 complete kafka stream-query service

---
+++ kafka/stream-query/src/main/java/com/networknt/kafka/UserQueryStreams.java
@@ -0,0 +1,106 @@
+package com.networknt.kafka;
+
+import com.networknt.config.Config;
+import com.networknt.kafka.common.KafkaStreamsConfig;
+import com.networknt.kafka.streams.LightStreams;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StoreQueryParameters;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.processor.AbstractProcessor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.state.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+public class UserQueryStreams implements LightStreams {
+    static private final Logger logger = LoggerFactory.getLogger(UserQueryStreams.class);
+    private static final String APP = "userQuery";
+
+    static private Properties streamsProps;
+    static final KafkaStreamsConfig config = (KafkaStreamsConfig) Config.getInstance().getJsonObjectConfig(KafkaStreamsConfig.CONFIG_NAME, KafkaStreamsConfig.class);
+
+    static {
+        streamsProps = new Properties();
+        streamsProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBootstrapServers());
+        streamsProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    }
+
+    private static final String userId = "user-id-store"; // this is a local store from userId to user object.
+
+    KafkaStreams userQueryStreams;
+
+    public UserQueryStreams() {
+        logger.info("UserQueryStreams is created");
+    }
+
+    public ReadOnlyKeyValueStore<String, String> getUserIdStore() {
+        return userQueryStreams.store(StoreQueryParameters.fromNameAndType(userId, QueryableStoreTypes.keyValueStore()));
+    }
+
+    public StreamsMetadata getUserIdStreamsMetadata(String id) {
+        return userQueryStreams.metadataForKey(userId,  id, Serdes.String().serializer());
+    }
+
+    private void startUserQueryStreams(String ip, int port) {
+
+        StoreBuilder<KeyValueStore<String, String>> keyValueUserIdStoreBuilder =
+                Stores.keyValueStoreBuilder(Stores.persistentKeyValueStore(userId),
+                        Serdes.String(),
+                        Serdes.String());
+
+        final Topology topology = new Topology();
+        topology.addSource("SourceTopicProcessor", "test");
+        topology.addProcessor("UserEventProcessor", UserEventProcessor::new, "SourceTopicProcessor");
+        topology.addStateStore(keyValueUserIdStoreBuilder, "UserEventProcessor");
+
+        streamsProps.put(StreamsConfig.APPLICATION_ID_CONFIG, "user-query");
+        streamsProps.put(StreamsConfig.APPLICATION_SERVER_CONFIG, ip + ":" + port);
+        userQueryStreams = new KafkaStreams(topology, streamsProps);
+        if(config.isCleanUp()) {
+            userQueryStreams.cleanUp();
+        }
+        userQueryStreams.start();
+    }
+
+    public static class UserEventProcessor extends AbstractProcessor<byte[], byte[]> {
+
+        private ProcessorContext pc;
+        private KeyValueStore<String, String> userIdStore;
+
+        public UserEventProcessor() {
+        }
+
+        @Override
+        public void init(ProcessorContext pc) {
+            this.pc = pc;
+            this.userIdStore = (KeyValueStore<String, String>) pc.getStateStore(userId);
+            if (logger.isInfoEnabled()) logger.info("Processor initialized");
+        }
+
+        @Override
+        public void process(byte[] key, byte[] value) {
+            String userId = new String(key, StandardCharsets.UTF_8);
+            String userStr = new String(value, StandardCharsets.UTF_8);
+            if(logger.isTraceEnabled()) logger.trace("Event = " + userStr);
+            userIdStore.put(userId, userStr);
+        }
+    }
+
+    @Override
+    public void start(String ip, int port) {
+        if(logger.isDebugEnabled()) logger.debug("userQueryStreams is starting...");
+        startUserQueryStreams(ip, port);
+    }
+
+    @Override
+    public void close() {
+        if(logger.isDebugEnabled()) logger.debug("userQueryStreams is closing...");
+        userQueryStreams.close();
+    }
+}
--- kafka/stream-query/src/main/java/com/networknt/kafka/handler/QueryUserIdGetHandler.java
+++ kafka/stream-query/src/main/java/com/networknt/kafka/handler/QueryUserIdGetHandler.java
@@ -1,19 +1,113 @@
 package com.networknt.kafka.handler;
 
+import com.networknt.client.Http2Client;
+import com.networknt.config.Config;
+import com.networknt.config.JsonMapper;
 import com.networknt.handler.LightHttpHandler;
+import com.networknt.kafka.UserQueryStartupHook;
+import com.networknt.monad.Failure;
+import com.networknt.monad.Result;
+import com.networknt.monad.Success;
+import com.networknt.server.Server;
+import com.networknt.status.Status;
+import com.networknt.utility.NetUtils;
+import com.networknt.utility.NioUtils;
+import io.undertow.UndertowOptions;
+import io.undertow.client.ClientConnection;
+import io.undertow.client.ClientRequest;
+import io.undertow.client.ClientResponse;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 import io.undertow.util.HttpString;
+import io.undertow.util.Methods;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.apache.kafka.streams.state.StreamsMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xnio.OptionMap;
+
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
 For more information on how to write business handlers, please check the link below.
 https://doc.networknt.com/development/business-handler/rest/
 */
 public class QueryUserIdGetHandler implements LightHttpHandler {
-    
+    private static final Logger logger = LoggerFactory.getLogger(QueryUserIdGetHandler.class);
+    static Map<String, ClientConnection> connCache = new ConcurrentHashMap<>();
+    static Http2Client client = Http2Client.getInstance();
+
+    private static final String OBJECT_NOT_FOUND = "ERR11637";
+    private static final String GENERIC_EXCEPTION = "ERR10014";
+
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        exchange.endExchange();
+        String userId = exchange.getQueryParameters().get("userId").getFirst();
+        if(logger.isTraceEnabled()) logger.trace("userId = " + userId);
+
+        ReadOnlyKeyValueStore<String, String> keyValueStore = UserQueryStartupHook.streams.getUserIdStore();
+        String data = keyValueStore.get(userId);
+        if(data != null) {
+            exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, "application/json");
+            exchange.getResponseSender().send(data);
+        } else {
+            StreamsMetadata metadata = UserQueryStartupHook.streams.getUserIdStreamsMetadata(userId);
+            if(logger.isDebugEnabled()) logger.debug("found address in another instance " + metadata.host() + ":" + metadata.port());
+            String url = "https://" + metadata.host() + ":" + metadata.port();
+            if(NetUtils.getLocalAddressByDatagram().equals(metadata.host()) && Server.config.getHttpsPort() == metadata.port()) {
+                setExchangeStatus(exchange, OBJECT_NOT_FOUND, "user", userId);
+                return;
+            } else {
+                Result<String> resultStatus = getUserById(userId, exchange, url);
+                if (resultStatus.isSuccess()) {
+                    exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, "application/json");
+                    exchange.getResponseSender().send(resultStatus.getResult());
+                    return;
+                }
+            }
+            setExchangeStatus(exchange, OBJECT_NOT_FOUND, "user", userId);
+            return;
+        }
     }
+
+    public static Result<String> getUserById(String userId, HttpServerExchange exchange, String url) {
+        Result<String> result = null;
+        try {
+            ClientConnection conn = connCache.get(url);
+            if(conn == null || !conn.isOpen()) {
+                conn = client.connect(new URI(url), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.create(UndertowOptions.ENABLE_HTTP2, true)).get();
+                connCache.put(url, conn);
+            }
+            // Create one CountDownLatch that will be reset in the callback function
+            final CountDownLatch latch = new CountDownLatch(1);
+            // Create an AtomicReference object to receive ClientResponse from callback function
+            final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+            String path = "/query/" + userId;
+            final ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath(path);
+            String token = exchange.getRequestHeaders().getFirst(Headers.AUTHORIZATION);
+            if(token != null) request.getRequestHeaders().put(Headers.AUTHORIZATION, token);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            conn.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+            int statusCode = reference.get().getResponseCode();
+            String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+            if(statusCode != 200) {
+                Status status = Config.getInstance().getMapper().readValue(body, Status.class);
+                result = Failure.of(status);
+            } else result = Success.of(body);
+        } catch (Exception e) {
+            logger.error("Exception:", e);
+            Status status = new Status(GENERIC_EXCEPTION, e.getMessage());
+            result = Failure.of(status);
+        }
+        return result;
+    }
+
 }
