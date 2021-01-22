e79b46c036930442f1c0635a7983b183e3941edd
Steve Hu <stevehu@gmail.com>
2021-01-22 14:14:03 -0500

    fixes #650 update stream-report example

---
+++ kafka/stream-report/src/main/java/com/networknt/kafka/UserReportStartup.java
@@ -0,0 +1,21 @@
+package com.networknt.kafka;
+
+import com.networknt.server.Server;
+import com.networknt.server.StartupHookProvider;
+import com.networknt.utility.NetUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserReportStartup implements StartupHookProvider {
+    static final Logger logger = LoggerFactory.getLogger(UserReportStartup.class);
+    public static UserReportStreams streams = null;
+    @Override
+    public void onStartup() {
+        int port = Server.config.getHttpsPort();
+        String ip = NetUtils.getLocalAddressByDatagram();
+        logger.info("ip = " + ip + " port = " + port);
+        streams = new UserReportStreams();
+        // start the kafka stream process
+        streams.start(ip, port);
+    }
+}
