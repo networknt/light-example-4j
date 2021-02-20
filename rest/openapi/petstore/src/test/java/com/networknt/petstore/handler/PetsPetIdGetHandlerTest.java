e56051f73e9c18638236364bde6b7e3c285d368b
Steve Hu <stevehu@gmail.com>
2021-02-20 14:28:13 -0500

    fixes #669 regenerate openapi petstore for jackson upgrade

--- rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsPetIdGetHandlerTest.java
+++ rest/openapi/petstore/src/test/java/com/networknt/petstore/handler/PetsPetIdGetHandlerTest.java
@@ -58,7 +58,7 @@ public class PetsPetIdGetHandlerTest {
             throw new ClientException(e);
         }
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
-        String requestUri = "/v1/pets/hqLVpSWEQbNuSGXojtfZhKkESGKlf";
+        String requestUri = "/v1/pets/MduwaFTTlYgipOUIvEvsyUsTNXl";
         String httpMethod = "get";
         try {
             ClientRequest request = new ClientRequest().setPath(requestUri).setMethod(Methods.GET);
