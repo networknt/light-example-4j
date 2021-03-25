d3eeeff0367ff5f966b053bf3d1c6e04ba38a03e
Steve Hu <stevehu@gmail.com>
2021-03-25 13:00:48 -0400

    fixes #672 update the debug to output record clearly

--- kafka/sidecar-backend/src/main/java/com/networknt/mesh/kafka/backend/handler/KafkaRecordsPostHandler.java
+++ kafka/sidecar-backend/src/main/java/com/networknt/mesh/kafka/backend/handler/KafkaRecordsPostHandler.java
@@ -4,7 +4,12 @@ import com.networknt.body.BodyHandler;
 import com.networknt.config.JsonMapper;
 import com.networknt.handler.LightHttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.websockets.core.protocol.version07.UTF8Checker;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 
 /**
 For more information on how to write business handlers, please check the link below.
@@ -14,9 +19,18 @@ public class KafkaRecordsPostHandler implements LightHttpHandler {
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        List<String> body = (List<String>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
-        logger.info("received call from the sidecar: " + JsonMapper.toJson(body));
-        List<Map<String, Object>> records = JsonMapper.
+        List<Map<String, Object>> records = (List<Map<String, Object>>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
+        for(int i = 0; i < records.size(); i++) {
+            Map<String, Object> record = records.get(i);
+            logger.debug("partition = " + record.get("partition"));
+            logger.debug("offset = " + record.get("offset"));
+            logger.debug("key = " + record.get("key"));
+            logger.debug("value = " + record.get("value"));
+            Map<String, String> headerMap = (Map<String, String>)record.get("headers");
+            headerMap.entrySet().forEach(entry -> {
+                logger.debug("header key = " + entry.getKey() + " header value = " + entry.getValue());
+            });
+        }
         exchange.setStatusCode(201);
         exchange.endExchange();
     }
