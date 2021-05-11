3046c982abfdae109bc1ebca8e941f1de6955569
Steve Hu <stevehu@gmail.com>
2021-05-10 20:10:45 -0400

    fixes #674 update the sidecar backend for dead letter response

--- kafka/sidecar-backend/src/main/java/com/networknt/mesh/kafka/backend/handler/KafkaRecordsPostHandler.java
+++ kafka/sidecar-backend/src/main/java/com/networknt/mesh/kafka/backend/handler/KafkaRecordsPostHandler.java
@@ -1,8 +1,16 @@
 package com.networknt.mesh.kafka.backend.handler;
 
 import com.networknt.body.BodyHandler;
+import com.networknt.config.Config;
+import com.networknt.config.JsonMapper;
 import com.networknt.handler.LightHttpHandler;
+import com.networknt.kafka.entity.ConsumerRecord;
+import com.networknt.kafka.entity.RecordProcessedResult;
 import io.undertow.server.HttpServerExchange;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,22 +22,41 @@ public class KafkaRecordsPostHandler implements LightHttpHandler {
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
+        List<RecordProcessedResult> results = new ArrayList<>();
         List<Map<String, Object>> records = (List<Map<String, Object>>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
         for(int i = 0; i < records.size(); i++) {
-            Map<String, Object> record = records.get(i);
-            logger.debug("topic = " + record.get("topic"));
-            logger.debug("partition = " + record.get("partition"));
-            logger.debug("offset = " + record.get("offset"));
-            logger.debug("key = " + record.get("key"));
-            logger.debug("value = " + record.get("value"));
-            Map<String, String> headerMap = (Map<String, String>)record.get("headers");
+            ConsumerRecord record = Config.getInstance().getMapper().convertValue(records.get(i), ConsumerRecord.class);
+            logger.debug("topic = " + record.getTopic());
+            logger.debug("partition = " + record.getPartition());
+            logger.debug("offset = " + record.getOffset());
+            logger.debug("key = " + record.getKey());
+            logger.debug("value = " + record.getValue());
+            Map<String, String> headerMap = record.getHeaders();
             if(headerMap != null && headerMap.size() > 0) {
                 headerMap.entrySet().forEach(entry -> {
                     logger.debug("header key = " + entry.getKey() + " header value = " + entry.getValue());
                 });
             }
+
+            if(i == 1) {
+                // simulate runtime exception that causes record cannot be processed successfully and ask
+                // sidecar to put the record to the dead letter queue.
+                try {
+                    int j = 10 / 0;
+                } catch (RuntimeException e) {
+                    StringWriter sw = new StringWriter();
+                    PrintWriter pw = new PrintWriter(sw);
+                    e.printStackTrace(pw);
+                    RecordProcessedResult rpr = new RecordProcessedResult(record, false, sw.toString());
+                    results.add(rpr);
+                }
+            } else {
+                // simulate processed successfully.
+                RecordProcessedResult rpr = new RecordProcessedResult(record, true, null);
+                results.add(rpr);
+            }
         }
-        exchange.setStatusCode(201);
-        exchange.endExchange();
+        exchange.setStatusCode(200);
+        exchange.getResponseSender().send(JsonMapper.toJson(results));
     }
 }
