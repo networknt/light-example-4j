0618b6b9e01295d6882ae7105c6b5fd680cb0462
Steve Hu <stevehu@gmail.com>
2023-06-13 08:57:20 -0400

    fixes #732 add log application for springboot

---
+++ springboot/log/src/test/java/com/networknt/log/LogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networknt.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
