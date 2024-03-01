d9108c3216478e1489c943896a530e6531da1851
Steve Hu <stevehu@gmail.com>
2024-03-01 17:28:48 -0500

    fixes #740 update client pool test dependency and add post request

--- client/pool-test/src/main/java/com/networknt/client/Http2ClientPool.java
+++ client/pool-test/src/main/java/com/networknt/client/Http2ClientPool.java
@@ -28,10 +28,12 @@ public class Http2ClientPool {
         //System.setProperty(DISABLE_HTTPS_ENDPOINT_IDENTIFICATION_PROPERTY, "true");
         client = Http2Client.getInstance();
         Http2ClientPool e = new Http2ClientPool();
-        for(int i = 0; i < 10000; i++) {
-            e.testHttp2Get();
-            Thread.sleep(1000);
-        }
+//        for(int i = 0; i < 10000; i++) {
+//            e.testHttp2Get();
+//            Thread.sleep(1000);
+//        }
+        // e.testHttp2Post();
+        e.testHttp2Get();
         System.exit(0);
     }
 
@@ -46,7 +48,7 @@ public class Http2ClientPool {
         // Create one CountDownLatch that will be reset in the callback function
         final CountDownLatch latch = new CountDownLatch(1);
         SimpleConnectionHolder.ConnectionToken connectionToken = null;
-        URI uri = new URI("https://lucky:9443");
+        URI uri = new URI("https://localhost:8443");
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
         try {
             connectionToken = client.borrow(uri, Http2Client.WORKER, client.getDefaultXnioSsl(), Http2Client.BUFFER_POOL, OptionMap.create(UndertowOptions.ENABLE_HTTP2, true));
@@ -62,4 +64,29 @@ public class Http2ClientPool {
         String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
         System.out.println("statusCode = " + statusCode + " body = " + body);
     }
+
+    public void testHttp2Post() throws Exception {
+        // Create one CountDownLatch that will be reset in the callback function
+        final CountDownLatch latch = new CountDownLatch(1);
+        SimpleConnectionHolder.ConnectionToken connectionToken = null;
+        URI uri = new URI("https://localhost:8443");
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        String requestBody = "{\"id\":1,\"name\":\"doggie\"}";
+        try {
+            connectionToken = client.borrow(uri, Http2Client.WORKER, client.getDefaultXnioSsl(), Http2Client.BUFFER_POOL, OptionMap.create(UndertowOptions.ENABLE_HTTP2, true));
+            ClientConnection connection = (ClientConnection) connectionToken.getRawConnection();
+            final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath("/v1/pets");
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/json");
+            request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+            connection.sendRequest(request, client.createClientCallback(reference, latch, requestBody));
+            latch.await(100, TimeUnit.MILLISECONDS);
+        } finally {
+            client.restore(connectionToken);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        System.out.println("statusCode = " + statusCode + " body = " + body);
+    }
+
 }
